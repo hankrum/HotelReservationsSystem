fa15ac91b9478ab46c40488c271cc38ad7ea43e8
hankrum <st_kolev@mail.bg>
2017-09-29 22:13:06 +0300

    Abstraction, Contracts

---
+++ HotelReservations.Data.Model/Contracts/IAuditable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HotelReservations.Data.Model.Contracts
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
